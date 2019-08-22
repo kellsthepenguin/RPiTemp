769f115e5a19d61664fd1306a55edc97cd1a439c
jwkim101201 <jwkim101201@gmail.com>
2019-08-22 23:32:49 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const { exec } = require('child_process') // importing child_process.exec only (saving memory)
+const mail = require('nodemailer') // importing nodemailer
+const file = require('file/smtpconfig.jsonc')
+
+function lol () {
+  exec('/opt/vc/bin/vcgencmd measure_temp| sed "s/[^0-9.]//g"', (err, stdout) => { // getting raspberry pi's temputure
+    if (err) {
+      console.log(err) // error handle
+    }
+    let transporter = mail.createTransport({
+      service: 'gmail',
+      auth: {
+        user: file.user, // your gmail,
+        pass: file.pass // your google password
+      }
+    })
+    let mailOpt = {
+      from: file.user,
+      to: file.receiver,
+      subject: file.subject,
+      text: `RaspberryPI Temputure: ${stdout}°C` // sending temp
+    }
+    transporter.sendMail(mailOpt, function (err, info) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('email sucessfully sent:', info)
+      }
+    })
+  })
+}
+setInterval(lol, 3600000) // sending mail each 1hour (1hour = 3600000ms)
